e16900828f31f7545dd2526833b27e970c6d7680
Dayner-Garcia <dayner1227garcia@gmail.com>
2025-04-12 11:31:12 -0400

    Init proyect

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
 import './global.css';
+import {Text, View} from "react-native";
 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx" />
-      <StatusBar style="auto" />
-    </>
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-blue-600 text-xl font-bold">Home del proyecto final</Text>
+      </View>
   );
 }
