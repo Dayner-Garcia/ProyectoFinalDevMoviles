4733151609655ad2648a0d18f1027dacd180f6f6
AjBonifacio <angelojose383@gmail.com>
2025-04-20 12:48:58 -0400

    feat: pantalla video

--- screens/videos/VideosScreen.tsx
+++ screens/videos/VideosScreen.tsx
@@ -1,9 +1,79 @@
-import { Text, View } from "react-native";
+import { Text, View,FlatList,SafeAreaView,StyleSheet } from "react-native";
+import { videos } from "types/Videos/videos";
+import { obtenerVideos } from "services/videosService";
+import React,{useState,useEffect} from "react";
+import WebView from "react-native-webview";
 
 export default function VideosScreen() {
+     
+    const [video,setVideos]= useState<videos[]>([]);
+
+
+    useEffect(()=>{
+        const fecthVideos= async () =>{
+            try{
+                   const res= await obtenerVideos()
+                   setVideos(res.datos)
+            }
+            catch(e){
+                console.error("error",e)
+
+            }
+        }
+    fecthVideos()})
+
+
     return (
-        <View className="flex-1 bg-white items-center justify-center">
-            <Text className="text-xl font-bold">Videos</Text>
-        </View>
+       <SafeAreaView style={style.container}>
+                    <FlatList 
+                        data={video}
+                        renderItem={({item})=>{
+                            return(
+                                <View style={style.card}>
+                                    <Text style={style.titulo}>{item.titulo}</Text>
+                                    <WebView style={style.video} source={{uri:`https://www.youtube.com/embed/${item.link}`}}/>
+                                    <Text style={style.contenido}>{item.descripcion}</Text>
+                                    <Text style={{textAlign:'right', fontSize:9, margin:16}}>{item.fecha}</Text>
+                                </View> 
+                            )
+                        }}
+                        
+                        
+                    />
+
+       </SafeAreaView>
     );
 }
+
+const style = StyleSheet.create({
+    container:{ 
+        flex: 1,
+        backgroundColor: "#f9f9f9",
+        justifyContent: "center",},
+        card:{
+            alignSelf:"center",
+            margin:10,
+            borderRadius:16,
+            width:'95%',
+            backgroundColor:"#eeeeee",
+            elevation:5},
+        titulo:{
+            fontSize:18,
+            fontWeight:'bold',
+            textAlign:'center'
+        },
+        contenido:{
+            textAlign:'justify',
+            margin:16
+            
+            
+        },
+        video:{
+            width:"95%",
+            height:150,
+            alignSelf:'center',
+            borderRadius:16,
+            marginVertical:15
+    
+        }
+})
--- services/videosService.ts
+++ services/videosService.ts
@@ -1,6 +1,6 @@
 import api from "../api/api";
 
-export const obtenerVideos = async () => {
+export const obtenerVideos = async (): Promise<{exito:boolean;datos:[],mensaje:string}> => {
     const response = await api.get('videos.php');
     return response.data;
 };
